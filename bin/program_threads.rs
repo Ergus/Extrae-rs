6b602e4da9180894496f46baa51a7b8ec957df41
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2024-12-28 02:19:58 +0100

    Add single thread program.

---
+++ bin/program_threads.rs
@@ -0,0 +1,28 @@
+use extrae_rs::{instrument_function, GlobalInfo, ThreadInfo, profile};
+
+#[profile]
+fn myfunction(i: u32) -> u32
+{
+    std::thread::sleep(std::time::Duration::from_millis(10));
+    i
+}
+
+fn main() -> nix::Result<()>
+{
+    println!("Start Program");
+
+    let handle = std::thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", myfunction(i));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", myfunction(i));
+    }
+
+    handle.join().unwrap();
+
+    println!("Done");
+    Ok(())
+}
